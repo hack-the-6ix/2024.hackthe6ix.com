648ba049ba72113ffa9db426f8eefab547bfc64a
fpunny <frederic.pun@gmail.com>
2024-03-13 16:55:29 -0400

    owo

--- src/app/(landing)/@hero/page.tsx
+++ src/app/(landing)/@hero/page.tsx
@@ -1,11 +1,19 @@
+import Container from '@/components/Container';
+import Flex from '@/components/Flex';
 import IconButton from '@/components/IconButton';
+import Text from '@/components/Text';
+import config from '@/config';
 
 function Hero() {
+  const range = new Intl.DateTimeFormat('en-CA', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }).formatRange(config.startAt, config.endAt);
   return (
-    <div>
-      hero
-      <IconButton icon="home" />
-    </div>
+    <Container innerProps={{ as: Flex, direction: 'column ' }}>
+      <Text>{range.replace('–', ' – ')} • In-person event</Text>
+    </Container>
   );
 }
 
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export default {
+  startAt: new Date('2024-08-18T00:00:01.000-04:00'),
+  endAt: new Date('2024-08-20T23:59:59.000-04:00'),
+};
