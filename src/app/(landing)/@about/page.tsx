fa4d62e971110d21a77c0829565dec3ff01e1b7d
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-16 22:23:05 -0700

    add color scssfor join us

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,17 +1,18 @@
 import Container from '../../../components/Container';
-
+import './about.scss';
 
 
 function AboutPage() {
   return (
-    <Container fluid> {/* Add the fluid prop */}
-      <h2>Join us for a weekend of memories.</h2>
-      <p>Hack the 6ix is the largest summer student-run, 
-        not-for-profit hackathon now in its tenth iteration, based in Toronto 
-        Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its tenth iteration, based in Toronto. We take pride in the diversity and talent of our hackers, who help us become a key player in the Toronto tech ecosystem. 
-        We provide an outlet for students to present their ideas of the future.
-        </p>
-    </Container>
+    <Container fluid> 
+    <h2><span className="highlight">Join us</span> for a weekend of memories.</h2>
+    <p>Hack the 6ix is the largest summer student-run, 
+      not-for-profit hackathon now in its tenth iteration, based in Toronto 
+      Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its tenth iteration, based in Toronto.
+      We take pride in the diversity and talent of our hackers, who help us become a key player in the Toronto tech ecosystem. 
+      We provide an outlet for students to present their ideas of the future.
+    </p>
+  </Container>
   );
 }
 
