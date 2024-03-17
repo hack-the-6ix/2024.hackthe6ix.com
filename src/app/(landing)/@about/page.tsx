c48b9a5901cd3b9b52dfbb9adefa22f99c1f0244
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-16 22:12:49 -0700

    Added header and text

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,10 +1,18 @@
-function About() {
+import Container from '../../../components/Container';
+
+
+
+function AboutPage() {
   return (
-    <div>
-      <h2>Hello, this is the about page.</h2>
-      <p>This is some additional content about the about page.</p>
-    </div>
+    <Container fluid> {/* Add the fluid prop */}
+      <h2>Join us for a weekend of memories.</h2>
+      <p>Hack the 6ix is the largest summer student-run, 
+        not-for-profit hackathon now in its tenth iteration, based in Toronto 
+        Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its tenth iteration, based in Toronto. We take pride in the diversity and talent of our hackers, who help us become a key player in the Toronto tech ecosystem. 
+        We provide an outlet for students to present their ideas of the future.
+        </p>
+    </Container>
   );
 }
 
-export default About;
+export default AboutPage;
