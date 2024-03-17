a2f66d1f41f830801ce5433269dd85ac8675ca43
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-16 21:47:47 -0700

    Just testing

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,5 +1,10 @@
 function About() {
-  return <div>about</div>;
+  return (
+    <div>
+      <h2>Hello, this is the about page.</h2>
+      <p>This is some additional content about the about page.</p>
+    </div>
+  );
 }
 
 export default About;
