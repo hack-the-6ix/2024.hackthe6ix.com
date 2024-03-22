a5af486e6d597ded81245f8254542ffb78cb2a0d
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-22 16:39:28 -0700

    changed scss file to module.scss

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,18 +1,24 @@
+import React from 'react';
 import Container from '../../../components/Container';
-import './about.scss';
-
+import Flex from '../../../components/Flex';
+import styles from './page.module.scss';
 
 function AboutPage() {
   return (
-    <Container fluid> 
-    <h2><span className="highlight">Join us</span> for a weekend of memories.</h2>
-    <p>Hack the 6ix is the largest summer student-run, 
-      not-for-profit hackathon now in its tenth iteration, based in Toronto 
-      Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its tenth iteration, based in Toronto.
-      We take pride in the diversity and talent of our hackers, who help us become a key player in the Toronto tech ecosystem. 
-      We provide an outlet for students to present their ideas of the future.
-    </p>
-  </Container>
+    <Container>
+      <Flex className={styles.container} direction="column" align="center">
+        <h2>
+          <span className="highlight">Join us</span> for a weekend of memories.
+        </h2>
+        <p>
+          Hack the 6ix is the largest summer student-run, not-for-profit
+          hackathon now in its tenth iteration, based in Toronto. We take pride
+          in the diversity and talent of our hackers, who help us become a key
+          player in the Toronto tech ecosystem. We provide an outlet for
+          students to present their ideas of the future.
+        </p>
+      </Flex>
+    </Container>
   );
 }
 
