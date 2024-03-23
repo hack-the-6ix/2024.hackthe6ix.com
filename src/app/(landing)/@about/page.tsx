491fbf55244700e8ef70089f80600a70a4d201ff
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-22 17:01:57 -0700

    adding background layout

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import Container from '../../../components/Container';
-import Flex from '../../../components/Flex';
+import Container from '@/components/Container';
+import Flex from '@/components/Flex';
 import styles from './about.module.scss';
 
 function AboutPage() {
   return (
+    <Container className={styles.frame}>
+      <Flex direction="column" className={styles.container} gap="2x-big">
+        <p>Test</p>
+      </Flex>
+    </Container>
   );
 }
 
 export default AboutPage;
-
-
-
-
-{/* <Container>
-<Flex className={styles.container} direction="column" align="center">
-  <h2>
-    <span className="highlight">Join us</span> for a weekend of memories.
-  </h2>
-  <p>
-    Hack the 6ix is the largest summer student-run, not-for-profit
-    hackathon now in its tenth iteration, based in Toronto. We take pride
-    in the diversity and talent of our hackers, who help us become a key
-    player in the Toronto tech ecosystem. We provide an outlet for
-    students to present their ideas of the future.
-  </p>
-</Flex>
-</Container> */}
