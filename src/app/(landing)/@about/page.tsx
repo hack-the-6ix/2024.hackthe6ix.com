113715179980ce2ad94c47c6af7c965a94f541da
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-22 17:41:49 -0700

    Header and text added

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Container from '@/components/Container';
 import Flex from '@/components/Flex';
+import Text from '@/components/Text';
 import styles from './about.module.scss';
 
 function AboutPage() {
   return (
     <Container className={styles.frame}>
       <Flex direction="column" className={styles.container} gap="2x-big">
-        <p>Test</p>
+        <Text textType="heading-sm" textColor="secondary-900">
+          <span className={styles.yellow}>Join us </span>
+          for a weekend of memories
+        </Text>
+        <Text
+          className={styles.textBox}
+          textType="paragraph-sm"
+          textColor="secondary-900"
+        >
+          Hack the 6ix is the largest summer student-run, not-for-profit
+          hackathon now in its tenth iteration, based in Toronto. We take pride
+          in the diversity and talent of our hackers, who help us become a key
+          player in the Toronto tech ecosystem. We provide an outlet for
+          students to present their ideas of the future.
+        </Text>
       </Flex>
     </Container>
   );
