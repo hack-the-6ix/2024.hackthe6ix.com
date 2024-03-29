b07b78421e9eced704c196eee25b10a8d0036dc8
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-03-28 18:11:13 -0700

    Deleting textbox from scss

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -18,19 +18,19 @@ function AboutPage() {
             for a weekend of memories
           </Text>
         </Text>
-
-        <Text
-          className={styles.textBox}
-          textType="paragraph-lg"
-          textColor="secondary-900"
-          textWeight="medium"
-        >
-          Hack the 6ix is the largest summer student-run, not-for-profit
-          hackathon now in its tenth iteration, based in Toronto. We take pride
-          in the diversity and talent of our hackers, who help us become a key
-          player in the Toronto tech ecosystem. We provide an outlet for
-          students to present their ideas of the future.
-        </Text>
+        <Flex align="center">
+          <Text
+            textType="paragraph-lg"
+            textColor="secondary-900"
+            textWeight="medium"
+          >
+            Hack the 6ix is the largest summer student-run, not-for-profit
+            hackathon now in its tenth iteration, based in Toronto. We take
+            pride in the diversity and talent of our hackers, who help us become
+            a key player in the Toronto tech ecosystem. We provide an outlet for
+            students to present their ideas of the future.
+          </Text>
+        </Flex>
       </Flex>
     </Container>
   );
