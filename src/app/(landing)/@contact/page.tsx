7dad8678eceed761d03af3cce3e2dd2ea10a2287
Gordon Lin <contact@gordonlin.ca>
2024-04-07 20:24:30 -0400

    Created simple footer with title and dynamic background

--- src/app/(landing)/@contact/page.tsx
+++ src/app/(landing)/@contact/page.tsx
@@ -1,5 +1,38 @@
+import Image from 'next/image';
+import Button from '@/components/Button';
+import Container from '@/components/Container';
+import Flex from '@/components/Flex';
+import Input from '@/components/Input';
+import Text from '@/components/Text';
+import bedForeground from './_assets/bed-foreground.svg?url';
+import styles from './layout.module.scss';
+
 function Contact() {
-  return <div>contact</div>;
+  return (
+    <>
+      <Container as="section" innerProps={{ className: styles.contact }}>
+        <Image
+          src={bedForeground}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center top"
+          className={styles.bgForeground}
+        />
+        <Flex direction="column" align="start" gap="2x-lg">
+          <Text
+            textColor="secondary-900"
+            textType="heading-lg"
+            textWeight="semi-bold"
+            textAlign="start"
+            as="h2"
+          >
+            Contact Us
+          </Text>
+        </Flex>
+      </Container>
+    </>
+  );
 }
 
 export default Contact;
