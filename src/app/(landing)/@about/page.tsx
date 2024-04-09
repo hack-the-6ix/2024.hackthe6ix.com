2ffe4b80a1fa0705e9fbdaf2d48765cd49fe0922
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-04-08 21:27:35 -0700

    Ivan- About section (#14)
    * Just testing
    
    * Added header and text
    
    * add color scssfor join us
    
    * changed scss file to module.scss
    
    * reorganizing
    
    * adding background layout
    
    * Header and text added
    
    * add assets folder
    
    * changes before edit submission
    
    * new branch: Fixing fonts + weights
    
    * changed font weights
    
    * add textWeight to header
    
    * syntax weight
    
    * finalized version
    
    * Deleting textbox from scss
    
    * Added Flex components
    
    * Add flex
    
    * clean up changes
    
    * html
    
    * section

--- src/app/(landing)/@about/page.tsx
+++ src/app/(landing)/@about/page.tsx
@@ -1,5 +1,52 @@
-function About() {
-  return <div>about</div>;
+import React from 'react';
+import Container from '@/components/Container';
+import Flex from '@/components/Flex';
+import Text from '@/components/Text';
+import styles from './about.module.scss';
+
+function AboutPage() {
+  return (
+    <Container as="section" className={styles.frame}>
+      <Flex
+        align="center"
+        direction="column"
+        className={styles.container}
+        gap="2x-big"
+      >
+        <Text
+          textType="heading-lg"
+          textColor="secondary-900"
+          textWeight="bold"
+          textAlign="center"
+          as="h2"
+        >
+          <span className={styles.yellow}>Join us </span>
+          <Text
+            textType="heading-lg"
+            textColor="secondary-900"
+            textWeight="semi-bold"
+            textAlign="center"
+          >
+            for a weekend of memories
+          </Text>
+        </Text>
+
+        <Text
+          textType="paragraph-lg"
+          textColor="secondary-900"
+          textWeight="medium"
+          textAlign="center"
+          as="p"
+        >
+          Hack the 6ix is the largest summer student-run, not-for-profit
+          hackathon now in its tenth iteration, based in Toronto. We take pride
+          in the diversity and talent of our hackers, who help us become a key
+          player in the Toronto tech ecosystem. We provide an outlet for
+          students to present their ideas of the future.
+        </Text>
+      </Flex>
+    </Container>
+  );
 }
 
-export default About;
+export default AboutPage;
