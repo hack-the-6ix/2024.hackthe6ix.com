7557737f93240010014a5c494e706180a332b3b3
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-05-22 20:22:29 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,38 @@ import Script from 'next/script';
 import '@/styles/index.scss';
 
 export const metadata: Metadata = {
-  title: 'Hack the 6ix',
+  title: 'Hack the 6ix | Torontos Largest Summer Hackathon',
   description:
-    'Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its ninth iteration, based in Toronto. Join hundreds of students from diverse backgrounds, disciplines, and skill levels to work together on projects that have real-world impact.',
+    'Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its 10th iteration, based in Toronto. Join hundreds of students from diverse backgrounds, disciplines, and skill levels to work together on projects that have real-world impact.',
+  keywords: ['hackathon', 'toronto', 'students', 'tech', 'innovation'],
+  openGraph: {
+    title: 'Hack the 6ix | Torontos Largest Summer Hackathon',
+    description:
+      'Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its 10th iteration, based in Toronto. Join hundreds of students from diverse backgrounds, disciplines, and skill levels to work together on projects that have real-world impact.',
+    url: 'https://hackthe6ix.com',
+    siteName: 'Hack the 6ix',
+    images: [
+      {
+        url: './banner.png',
+        width: 1200,
+        height: 630,
+        alt: 'Hack the 6ix banner showcasing diverse students collaborating on innovative projects.',
+      },
+    ],
+    locale: 'en_CA',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Hack the 6ix | Torontos Largest Summer Hackathon',
+    description:
+      'Hack the 6ix is the largest summer student-run, not-for-profit hackathon now in its 10th iteration, based in Toronto. Join hundreds of students from diverse backgrounds, disciplines, and skill levels to work together on projects that have real-world impact.',
+    images: ['./banner.png'],
+    creator: '@hackthe6ix',
+  },
+  icons: {
+    icon: './icon.png',
+  },
 };
 
 export default function RootLayout({
