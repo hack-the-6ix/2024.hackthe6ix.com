1dc46ec66bb0ee688b8fa48244d11526474b8373
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2024-06-18 04:01:39 -0400

    Added floating UI and animation
    - 2 boats in hero
    - a shark in sponsor
    - a turtle in testimony
    - 2 fishes in FAQ

--- src/components/Animate/index.tsx
+++ src/components/Animate/index.tsx
@@ -8,6 +8,9 @@ interface AnimateProps {
   delay?: number;
   initial?: Object;
   animate?: Object;
+  repeat?: number;
+  repeatType?: 'loop' | 'mirror' | 'reverse';
+  repeatDelay?: number;
 }
 
 export default function Animate({
@@ -16,13 +19,22 @@ export default function Animate({
   delay = 0,
   initial = { opacity: 0 },
   animate = { opacity: 1 },
+  repeat = 0,
+  repeatType = 'loop',
+  repeatDelay = 0,
 }: AnimateProps) {
   return (
     <AnimatePresence>
       <motion.div
         initial={initial}
         animate={animate}
-        transition={{ duration, delay }}
+        transition={{
+          duration,
+          delay,
+          repeat,
+          repeatType,
+          repeatDelay,
+        }}
       >
         {children}
       </motion.div>
